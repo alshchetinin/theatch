0cd3c3fe26890e88a509bdd530761ec005c4b998
alshchetinin <alexsander.qw@gmail.com>
2020-05-07 10:49:00 +0300

    first commit

--- src/theme/header.php
+++ src/theme/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
+   <meta charset="<?php bloginfo( 'charset' ); ?>" />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<header id="header" class="container header">
-	<h1><?php bloginfo( 'name' ); ?></h1>
-</header>
+   <header id="header">
+      <h1><?php bloginfo( 'name' ); ?></h1>
+   </header>
