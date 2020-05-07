e657919eb7a92813ca28255c8df26cd221e915c7
alshchetinin <alexsander.qw@gmail.com>
2020-05-07 21:18:33 +0300

    add main slider

--- src/theme/blocks/main-slider.php
+++ src/theme/blocks/main-slider.php
@@ -1,1 +1,124 @@
-<h1>fsdfsdf</h1>
+<section id="main-slider">
+   <div class="main-slider">
+      <div class="main-slider__item"
+         style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.png)">
+         <div class="main-slider__wrapper">
+            <div class="main-slider__content">
+               <div class="main-slider__subtitle">
+                  Стильный жилой комплекс рядом с лесом и парком
+               </div>
+
+               <div class="main-slider__title">
+                  Новая «Среда» для жизни
+               </div>
+               <div class="main-slider__arrow">
+                  <img src="<?php echo get_template_directory_uri()?>/img/svg/arrow-right.svg" alt="" width="70px">
+               </div>
+
+
+            </div>
+
+
+         </div>
+
+      </div>
+      <div class="main-slider__item"
+         style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.png)">
+         <div class="main-slider__wrapper">
+            <div class="main-slider__content">
+               <div class="main-slider__subtitle">
+                  Стильный жилой комплекс рядом с лесом и парком
+               </div>
+
+               <div class="main-slider__title">
+                  Новая «Среда» для жизни
+               </div>
+               <div class="main-slider__arrow">
+                  <img src="<?php echo get_template_directory_uri()?>/img/svg/arrow-right.svg" alt="" width="70px">
+               </div>
+
+
+            </div>
+
+
+         </div>
+
+      </div>
+      <div class="main-slider__item"
+         style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.png)">
+         <div class="main-slider__wrapper">
+            <div class="main-slider__content">
+               <div class="main-slider__subtitle">
+                  Стильный жилой комплекс рядом с лесом и парком
+               </div>
+
+               <div class="main-slider__title">
+                  Новая «Среда» для жизни
+               </div>
+               <div class="main-slider__arrow">
+                  <img src="<?php echo get_template_directory_uri()?>/img/svg/arrow-right.svg" alt="" width="70px">
+               </div>
+
+
+            </div>
+
+
+         </div>
+
+      </div>
+      <div class="main-slider__item"
+         style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.png)">
+         <div class="main-slider__wrapper">
+            <div class="main-slider__content">
+               <div class="main-slider__subtitle">
+                  Стильный жилой комплекс рядом с лесом и парком
+               </div>
+
+               <div class="main-slider__title">
+                  Новая «Среда» для жизни
+               </div>
+               <div class="main-slider__arrow">
+                  <img src="<?php echo get_template_directory_uri()?>/img/svg/arrow-right.svg" alt="" width="70px">
+               </div>
+
+
+            </div>
+
+
+         </div>
+
+      </div>
+      <div class="main-slider__item"
+         style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.png)">
+         <div class="main-slider__wrapper">
+            <div class="main-slider__content">
+               <div class="main-slider__subtitle">
+                  Стильный жилой комплекс рядом с лесом и парком
+               </div>
+
+               <div class="main-slider__title">
+                  Новая «Среда» для жизни
+               </div>
+               <div class="main-slider__arrow">
+                  <img src="<?php echo get_template_directory_uri()?>/img/svg/arrow-right.svg" alt="" width="70px">
+               </div>
+
+
+            </div>
+
+
+         </div>
+
+      </div>
+   </div>
+</section>
+
+<script>
+$('.main-slider').slick({
+   infinite: true,
+   easing: 'ease-in-out',
+   autoplay: true,
+   autoplaySpeed: 4500,
+   speed: 2000
+});
+</script>
