bcde83dc850645c9d08ef71dcff9803b4359ace6
alshchetinin <alexsander.qw@gmail.com>
2020-05-07 21:18:20 +0300

    add lead section

---
+++ src/theme/blocks/lead-section.php
@@ -0,0 +1,17 @@
+<section id="lead-section">
+   <div class="wrapper">
+      <div class="lead">
+         <div class="lead__title">
+            <h1>
+               <img src="<?php echo get_template_directory_uri()?>/img/svg/rhombus-gradient.svg" alt=""> The Architect
+               <br>Диджитал эксперт в&nbsp;девелопменте
+            </h1>
+
+         </div>
+         <div class="lead__picture">
+            <img src="<?php echo get_template_directory_uri()?>/img/svg/pic1.svg" alt="">
+         </div>
+
+      </div>
+   </div>
+</section>
