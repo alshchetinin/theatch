369a73ceab794114bad2c11333f8840411c3f0dd
alshchetinin <alexsander.qw@gmail.com>
2020-05-26 18:14:49 +0300

    add mobile project

--- src/theme/blocks/main-project.php
+++ src/theme/blocks/main-project.php
@@ -62,3 +62,61 @@
 
    </div>
 </section>
+<section id="main-project-mobile" class="main-project-mobile">
+   <div class="wrapper">
+      <div class="main-project-mobile-list">
+         <div class="title-section title-section_project">
+            <h2>Свежие кейсы </h2>
+         </div>
+         <div class="main-project-mobile-list-item">
+            <div class="main-project-mobile-list-item__img">
+               <img src="<?php echo get_template_directory_uri()?>/img/bg.png" alt="">
+            </div>
+            <div class="main-project-mobile-list-item__tag">Сайт</div>
+            <div class="main-project-mobile-list-item__title">ЖК «Среда»</div>
+            <div class="main-project-mobile-list-item__description">ЖК «Среда» — проект крупного девелопера  в Тюмени
+               Партнер-Строй,
+               который специализируется на экологичных проектах с малоэтажной застройкой.</div>
+            <div class="main-project-mobile-list-item__button"><a href="#" class="button">Смотреть<img
+                     src="<?php echo get_template_directory_uri()?>/img/svg/button-arrow.svg" alt=""></a></div>
+         </div>
+         <div class="main-project-mobile-list-item">
+            <div class="main-project-mobile-list-item__img">
+               <img src="<?php echo get_template_directory_uri()?>/img/bg.png" alt="">
+            </div>
+            <div class="main-project-mobile-list-item__tag">Сайт</div>
+            <div class="main-project-mobile-list-item__title">ЖК «Среда»</div>
+            <div class="main-project-mobile-list-item__description">ЖК «Среда» — проект крупного девелопера  в Тюмени
+               Партнер-Строй,
+               который специализируется на экологичных проектах с малоэтажной застройкой.</div>
+            <div class="main-project-mobile-list-item__button"><a href="#" class="button">Смотреть<img
+                     src="<?php echo get_template_directory_uri()?>/img/svg/button-arrow.svg" alt=""></a></div>
+         </div>
+         <div class="main-project-mobile-list-item">
+            <div class="main-project-mobile-list-item__img">
+               <img src="<?php echo get_template_directory_uri()?>/img/bg.png" alt="">
+            </div>
+            <div class="main-project-mobile-list-item__tag">Сайт</div>
+            <div class="main-project-mobile-list-item__title">ЖК «Среда»</div>
+            <div class="main-project-mobile-list-item__description">ЖК «Среда» — проект крупного девелопера  в Тюмени
+               Партнер-Строй,
+               который специализируется на экологичных проектах с малоэтажной застройкой.</div>
+            <div class="main-project-mobile-list-item__button"><a href="#" class="button">Смотреть<img
+                     src="<?php echo get_template_directory_uri()?>/img/svg/button-arrow.svg" alt=""></a></div>
+         </div>
+         <div class="main-project-mobile-list-item">
+            <div class="main-project-mobile-list-item__img">
+               <img src="<?php echo get_template_directory_uri()?>/img/bg.png" alt="">
+            </div>
+            <div class="main-project-mobile-list-item__tag">Сайт</div>
+            <div class="main-project-mobile-list-item__title">ЖК «Среда»</div>
+            <div class="main-project-mobile-list-item__description">ЖК «Среда» — проект крупного девелопера  в Тюмени
+               Партнер-Строй,
+               который специализируется на экологичных проектах с малоэтажной застройкой.</div>
+            <div class="main-project-mobile-list-item__button"><a href="#" class="button">Смотреть<img
+                     src="<?php echo get_template_directory_uri()?>/img/svg/button-arrow.svg" alt=""></a></div>
+         </div>
+      </div>
+   </div>
+
+</section>
